0a00d1674344c745f816da6e406f23a68d6a8fbf
Jon Wright <jon@streamdal.com>
2023-11-12 02:21:50 -0800

    added user content, stylized theme, added custom WIP component fixed description

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,21 +5,78 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: 'Minervian Dynamics',
+			customCss: [
+				// Relative path to your custom CSS file
+				'./src/styles/mindytheme.scss',
+				'@fontsource/titillium-web',
+			],
+			logo: {
+				src: './src/assets/logo.png',
+			},
 			social: {
-				github: 'https://github.com/withastro/starlight',
+				discord: 'https://discord.gg/jQnsy3qWu',
 			},
 			sidebar: [
 				{
-					label: 'Guides',
+					label: 'Getting Started',
 					items: [
 						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
+						{ 
+							label: 'Getting Started', 
+							link: '/guides/getting-started' 
+						},
+						{ 
+							label: 'Generating Isk', 
+							link: '/guides/generating-isk' 
+						},
+					],
+				},
+				{
+					label: 'Career Overviews',
+					/* This is pretty neat, you can have it auto-populate vs items[] array */
+					/* autogenerate: { directory: 'reference' }, <- that's pretty neat, you can  */
+					items: [
+						{
+							label: `Industrialist: Markets`,
+							link: `/career-overview/markets`
+						},
+						{
+							label: `Industrialist: Mining`,
+							link: `/career-overview/mining`
+						},
+						{
+							label: `Enforcer: Abyssals`,
+							link: `/career-overview/abyssals`
+						},
+						{
+							label: `Enforcer: Missions`,
+							link: `/career-overview/missions`
+						},
+						{
+							label: `Enforcer: Ratting`,
+							link: `/career-overview/ratting`
+						},
+						{
+							label: `Exploration`,
+							link: `/career-overview/exploration`
+						},
+						{
+							label: `SoF: Faction Warfare (FW)`,
+							link: `/career-overview/faction-warfare`
+						},
 					],
 				},
 				{
 					label: 'Reference',
-					autogenerate: { directory: 'reference' },
+					/* This is pretty neat, you can have it auto-populate vs items[] array */
+					/* autogenerate: { directory: 'reference' }, <- that's pretty neat, you can  */
+					items: [
+						{
+							label: `Useful Websites`,
+							link: `/reference/useful-websites`
+						},
+					],
 				},
 			],
 		}),
